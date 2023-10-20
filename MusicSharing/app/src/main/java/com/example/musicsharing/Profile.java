7aa39261d54a47685a668c5e07e4bf62b84ae182
Ashley348 <allison.348@osu.edu>
2023-10-19 20:30:03 -0400

    delete button added

--- MusicSharing/app/src/main/java/com/example/musicsharing/Profile.java
+++ MusicSharing/app/src/main/java/com/example/musicsharing/Profile.java
@@ -1,15 +1,24 @@
 package com.example.musicsharing;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Profile extends AppCompatActivity {
 
     private Button mEditProfileButton, mDeleteProfileButton;
+
 
 
     @Override
@@ -22,6 +31,7 @@ public class Profile extends AppCompatActivity {
 
         mDeleteProfileButton = (Button) findViewById(R.id.delete_profile_button);
 
+
         mEditProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +40,24 @@ public class Profile extends AppCompatActivity {
                 finish();
             }
         });
+
+        mDeleteProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("TAG", "Your account has been deleted.");
+                        }
+                    }
+                });
+            }
+        });
+
+
 
 
 
