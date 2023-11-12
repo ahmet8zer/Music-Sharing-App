78d8373e9ae22b42fa0408d4761d163fce488fac
Ahmet Ozer <ozer.18@osu.edu>
2023-11-12 14:56:42 -0500

    kinda sucks but I got it to add the users posts im gonna make it better soon

--- MusicSharing/app/src/main/java/com/example/musicsharing/ProfileActivity.java
+++ MusicSharing/app/src/main/java/com/example/musicsharing/ProfileActivity.java
@@ -16,9 +16,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -26,6 +33,17 @@ public class ProfileActivity extends AppCompatActivity {
     private Button mEditProfileButton, mDeleteProfileButton, mAddPostButton;
 
     private TextView mDisplayName;
+
+
+    private String song;
+    private String artist;
+    private String cover;
+
+    private FirebaseFirestore db;
+
+    private FirebaseUser user;
+
+    private Map<String, Object> post;
 
 
 
@@ -37,13 +55,45 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        db = FirebaseFirestore.getInstance();
 
         RecyclerView rv = findViewById(R.id.rvPosts);
         rv.setLayoutManager(new LinearLayoutManager(this));
         List<Post> posts = new ArrayList<Post>();
-        posts.add(new Post("Hello", "Adele, 25", "I really love this song because...", R.drawable.hello));
-        posts.add(new Post("Some Song", "This is what will happen if it doesn\'t have an image", "small caption", R.drawable.blankrecord));
-        posts.add(new Post("Hello", "Adele, 25", "large caption alkdjflkajsdlfkj asdkjflk asdklfj kals jdflkjalskd flj askdjflk asdjkf klajsdlkfjlksajlkdjf asjldfjk asdjk flkajsdlfjk akl sjdfjalkjs df jaslk djf kjaskldjfkljasdkjflkja sdkj fklajs dkl fjaklj sdfkj askd jfkjljasjflka sjdlkf jaskljd fkjlaskljdfaksjdfkjl asdkljfakj sd fljlkdas", R.drawable.hello));
+
+        //get the users posts
+        db.collection("users").document(user.getUid()).collection("posts").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        int size = queryDocumentSnapshots.size();
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            String uid = document.getString("uid");
+                            String author = document.getString("author");
+                            String title = document.getString("title");
+                            String caption = document.getString("caption");
+                            String artist = document.getString("artist");
+                            String cover = document.getString("cover");
+
+                            posts.add(new Post(title, artist, caption, R.drawable.hello));
+                        }
+
+                        rv.setAdapter(new MyAdapter(getApplicationContext(), posts));
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error getting documents.", e);
+                    }
+                });
+
+
+
+        //posts.add(new Post("Hello", "Adele, 25", "I really love this song because...", R.drawable.hello));
+        //posts.add(new Post("Some Song", "This is what will happen if it doesn\'t have an image", "small caption", R.drawable.blankrecord));
+        //posts.add(new Post("Hello", "Adele, 25", "large caption alkdjflkajsdlfkj asdkjflk asdklfj kals jdflkjalskd flj askdjflk asdjkf klajsdlkfjlksajlkdjf asjldfjk asdjk flkajsdlfjk akl sjdfjalkjs df jaslk djf kjaskldjfkljasdkjflkja sdkj fklajs dkl fjaklj sdfkj askd jfkjljasjflka sjdlkf jaskljd fkjlaskljdfaksjdfkjl asdkljfakj sd fljlkdas", R.drawable.hello));
 
         rv.setAdapter(new MyAdapter(getApplicationContext(), posts));
 
